4f012d1f47df2e24d158465c424a44420d010826
UfukGuler34 <ufuk.gulermega@gmail.com>
2022-01-03 19:06:38 +0300

    1

---
+++ main.js
@@ -0,0 +1,9 @@
+var bluePillid_el = document.getElementById('bluePillid');
+var redPillid_el = document.getElementById('redPillid');
+var penis_el = document.getElementById('peniid');
+
+function acsion() {
+    bluePillid_el.style.display = 'none';
+    redPillid_el.style.display = 'none';
+    penis_el.style.display = "block";
+}
